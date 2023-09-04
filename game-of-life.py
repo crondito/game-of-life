87f0bb39b762741f6dd298b479ea557446d100cd
Carlos Rodas <crods57@gmail.com>
2023-09-04 10:43:40 -0500

    added optimizations

--- game-of-life.py
+++ game-of-life.py
@@ -3,6 +3,11 @@ import sys
 import time
 import os
 
+LIVE_CELL = '\u2b1b'
+DEAD_CELL = '\u2b1c'
+
+NEIGHBOR_DIRECTIONS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
 def initialize_grid(rows=20, cols=40, density=0.2):
     grid = [[random.choices([0, 1], [1 - density, density])[0] for _ in range(cols)] for _ in range(rows)]
     return grid
@@ -10,24 +15,22 @@ def initialize_grid(rows=20, cols=40, density=0.2):
 def print_grid(grid):
     os.system('cls' if os.name == 'nt' else 'clear')
     for row in grid:
-        print(''.join(['\u2b1b' if cell else '\u2b1c' for cell in row]).encode('utf-8').decode(sys.stdout.encoding))
+        print(''.join([LIVE_CELL if cell else DEAD_CELL for cell in row]))
 
 def count_neighbors(grid, x, y):
     rows, cols = len(grid), len(grid[0])
     count = 0
-    for dx in [-1, 0, 1]:
-        for dy in [-1, 0, 1]:
-            if dx == 0 and dy == 0:
-                continue
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < rows and 0 <= ny < cols:
-                count += grid[nx][ny]
+    for dx, dy in NEIGHBOR_DIRECTIONS:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < rows and 0 <= ny < cols:
+            count += grid[nx][ny]
     return count
 
 def update_grid(grid):
-    new_grid = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
+    rows, cols = len(grid), len(grid[0])
+    new_grid = [[0] * cols for _ in range(rows)]
+    for i in range(rows):
+        for j in range(cols):
             neighbors = count_neighbors(grid, i, j)
             if grid[i][j]:
                 new_grid[i][j] = 1 if 2 <= neighbors <= 3 else 0
